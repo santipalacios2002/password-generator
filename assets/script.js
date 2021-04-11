eef89bffd628c444586b2b7fa8df7a3350a8c0d2
Santiago Palacios <santipalacios2002@gmail.com>
2021-04-10 23:03:44 -0400

    added functions for length, charact and number request

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//function that generates the password
+function generatePassword() {
+  requestLength();
+  requestSC();
+  requestNumbers();
+  console.log(`${lengthPassword}, ${specialCharacters} and ${numbers}`);
+  return 'yourpassword';
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
+
+// initializes length of password
+var lengthPassword;
+//function request character length
+function requestLength() {
+  lengthPassword = prompt('Please choose the length of the password (between 8-128)');
+  if ( lengthPassword > 128 || lengthPassword < 8 ) {
+    requestLength();
+  }
+}
+
+// initializes spacialCaracters boolean
+var specialCharacters;
+//funtion request if spacial characters
+function requestSC() {
+    specialCharacters = confirm('Press OK if you want special characters on your password')
+}
+
+var numbers;
+//funtion request if numbers
+function requestNumbers() {
+    numbers = confirm('Press OK if you want numbers on your password')
+}
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
