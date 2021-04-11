d54fff2fc222cc3ea0b28aef871c29e095717c25
Santiago Palacios <santipalacios2002@gmail.com>
2021-04-11 00:04:51 -0400

    added more logic to the password generation, added strings, working on if statements

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//special caracter string declared
+var specialCharacterStr = ' !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+
+//number string declared
+var numbersStr = '0123456789';
+
+//alphabet string declared
+var alphabetLowerCaseStr = 'abcdefghijklmnopqrstuvwxyz'
+
+//alphabet uppercase string declared
+var alphabetUpperCaseStr = alphabetLowerCaseStr.toUpperCase();
+
 //function that generates the password
 function generatePassword() {
   requestLength();
   requestSC();
   requestNumbers();
   console.log(`${lengthPassword}, ${specialCharacters} and ${numbers}`);
-  return 'yourpassword';
+  if (specialCharacters && numbers) { //if user wants special characters and numbers
+    var rndStr = specialCharacterStr.concat(numbersStr, alphabetLowerCaseStr, alphabetUpperCaseStr); //concatenates all the characters
+    console.log (rndStr);
+  } else if (specialCharacters) { //if user wants only special characters
+    var rndStr = specialCharacterStr.concat(alphabetLowerCaseStr, alphabetUpperCaseStr); //concatenates characters
+    console.log (rndStr);
+  } else if (numbers) { // if user wants only numbers
+    var rndStr = numbersStr.concat(alphabetLowerCaseStr, alphabetUpperCaseStr); //concatenates characters
+    console.log (rndStr);
+  }
+  var password = 'yourpassword';
+  return password;
 }
 
 // Write password to the #password input
@@ -29,11 +52,12 @@ function requestLength() {
 
 // initializes spacialCaracters boolean
 var specialCharacters;
-//funtion request if spacial characters
+//funtion request if special characters
 function requestSC() {
     specialCharacters = confirm('Press OK if you want special characters on your password')
 }
 
+// initializes numbers boolean
 var numbers;
 //funtion request if numbers
 function requestNumbers() {
